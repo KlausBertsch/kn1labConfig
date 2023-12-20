f76419cf524b0f42935cf9ab3ab8292c93e06136
KlausBertsch <80857787+KlausBertsch@users.noreply.github.com>
2023-12-20 10:31:25 +0100

    Update mailserver.sh

--- mailserver.sh
+++ mailserver.sh
@@ -1,19 +1,35 @@
-sudo ln -s /home/labrat/Maildir /workspaces/kn1lab/versuch1+2
+sudo apt-get update -y
+
+sudo apt-get update -y 
+sudo apt-get upgrade -y
+
+sudo apt-get install -y default-jdk
+sudo apt-get install -y maven
+sudo apt-get install -y dovecot-pop3d
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y postfix 
+sudo echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >>~/.bashrc
+sudo echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
+sudo echo "source ~/.bashrc"
+
+sudo mkdir -m 777 /home/ubuntu/kn1lab/Maildir
+sudo mkdir -m 777 /home/ubuntu/kn1lab/Maildir/new
+sudo mkdir -m 777 /home/ubuntu/kn1lab/Maildir/cur
+sudo mkdir -m 777 /home/ubuntu/kn1lab/Maildir/tmp   
 
 sudo rm -rf /etc/postfix
-sudo cp -r /workspaces/kn1lab/versuch1+2/postfix/postfix /etc
+sudo cp -r /home/ubuntu/kn1labConfig/postfix/postfix /etc
 sudo rm -rf /etc/dovecot
-sudo cp -r /workspaces/kn1lab/versuch1+2/postfix/dovecot /etc
+sudo cp -r /home/ubuntu/kn1labConfig/postfix/dovecot /etc
 sudo rm -rf /etc/aliases
-sudo cp -r /workspaces/kn1lab/versuch1+2/postfix/aliases /etc
+sudo cp -r /home/ubuntu/kn1labConfig/postfix/aliases /etc
 sudo rm -rf /etc/aliases.db
-sudo cp -r /workspaces/kn1lab/versuch1+2/postfix/aliases.db /etc
+sudo cp -r /home/ubuntu/kn1labConfig/postfix/aliases.db /etc
 sudo rm -rf /etc/mailcap
-sudo cp -r /workspaces/kn1lab/versuch1+2/postfix/mailcap /etc
+sudo cp -r /home/ubuntu/kn1labConfig/postfix/mailcap /etc
 sudo rm -rf /etc/mailcap.order
-sudo cp -r /workspaces/kn1lab/versuch1+2/postfix/mailcap.order /etc
+sudo cp -r /home/ubuntu/kn1labConfig/postfix/mailcap.order /etc
 sudo rm -rf /etc/mailname
-sudo cp -r /workspaces/kn1lab/versuch1+2/postfix/mailname /etc
+sudo cp -r /home/ubuntu/kn1labConfig/postfix/mailname /etc
 
 sudo service dovecot restart
 sudo service postfix restart
